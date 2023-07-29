ab4a5c3a7978d5217d6e1151fbf75beac3792287
nickdibartolo1 <107448423+nickdibartolo1@users.noreply.github.com>
2023-07-29 19:19:53 -0400

    added firebase backend, post request

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from './components/AddMovie';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
+
   //async OPTION
   // use async to eliminate .then() chains and make code a little more readable
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/', {})
+      const response = await fetch('https://udemy-http-923e3-default-rtdb.firebaseio.com/movies.json', {})
       if (!response.ok) {
         throw new Error('Something went wrong')
       }
       const data = await response.json();
 
+      const loadedMovies = [];
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episdoe_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }
-      })
-      setMovies(transformedMovies);
+        })
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+ async function addMovieHandler(movie) {
+    const response = await fetch('https://udemy-http-923e3-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+        });
+        const data = await response.json()
+    console.log(movie)
   }
 
   let content = <p>Found no movies.</p>
@@ -77,6 +97,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
